f5ca9cfcc10e3970f18d2f3bcca2d11059f7333d
Novikov.m@spglass.ru <Novikov.m@spglass.ru>
2018-10-26 18:16:59 +0300

    add new changes

--- src/components/widgets/FormFields/formFields.js
+++ src/components/widgets/FormFields/formFields.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React from "react";
 
-const FormFields = ({formdata, change, id}) => {
+const FormFields = ({ formdata, change, id }) => {
+    const showError = () => {
+        let errorMessage = null;
 
-  const renderTemplate = () => {
-    let formTemplate = null;
-  }
+        if (formdata.validation && !formdata.valid) {
+            errorMessage = (
+                <div className={StyleSheet.labelError}>
+                    {formdata.validationMessage}
+                </div>
+            );
+        }
+    };
 
-  return (
-    <div>
-      {renderTemplate()}
-    </div>
-  )
-}
+    const renderTemplate = () => {
+        let formTemplate = null;
+
+        switch (formdata.element) {
+        case "input":
+            formTemplate = (
+                <div>
+                    <input
+                        {...formdata.config}
+                        value={formdata.value}
+                        onBlur={event => change({ event, id, blur: true })}
+                        onChange={event =>
+                            change({ event, id, blur: false })
+                        }
+                    />
+                    {showError()}
+                </div>
+            );
+            break;
+        default:
+            formTemplate = null;
+        }
+        return formTemplate;
+    };
+
+    return <div>{renderTemplate()}</div>;
+};
 
 export default FormFields;
